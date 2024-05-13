f720fc1aeb8b57ead2307f7e2450be3efe5b3e8c
Oleg Anikin <tinenmi@gmail.com>
2024-05-13 10:32:34 +0300

    #23 add the ability to test the DOM

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
+//require('@testing-library/jest-dom')
+
 module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
+  testEnvironment: 'jsdom',
   testRegex: '/src/.*\\.spec?\\.ts$',
   moduleFileExtensions: ['ts', 'js'],
 }
--- src/spec/id.spec.ts
+++ src/spec/id.spec.ts
@@ -8,9 +8,19 @@ describe('id', () => {
   })
 
   it('if the mutable argument has been changed after the function call, the result should also change', () => {
-    let arg = { value: 1}
+    let arg = { value: 1 }
     let result = id(arg)
     arg.value = 2
     expect(result).toEqual({ value: 2 })
+  })
+
+  it('id works the same way with dom elements', () => {
+    document.body.innerHTML = `
+      <div id="root">Root there</div>
+    `
+
+    let root = document.getElementById('root')
+    let result = id(root)
+    expect(result).toBe(root)
   })
 })
